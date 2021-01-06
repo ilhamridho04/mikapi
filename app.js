895dc550212bf3d366c476ef43207c700f08ba2c
ilhamridho04 <45212532+ilhamridho04@users.noreply.github.com>
2021-01-06 19:45:20 +0700

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/url", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    res.json(["Ilham Ridho Asysyifa'a", "Teknik Informatika B 2017"]);
 });
 
 app.listen(3000, () => {
